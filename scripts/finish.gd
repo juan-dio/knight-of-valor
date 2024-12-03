b8d70ae9eb7814d6dd8c25504d7bf5e8179e4f09
juan-dio <juanaxl432@gmail.com>
2024-12-04 00:33:47 +0700

    feat(all): fix level 1-5, fix music, add win scene

--- scripts/finish.gd
+++ scripts/finish.gd
@@ -10,4 +10,4 @@ func _on_body_entered(body: Node2D) -> void:
 func _on_timer_timeout() -> void:
 	Global.freeze = false
 	Engine.time_scale = 1.0
-	get_tree().change_scene_to_file("res://scenes/death.tscn")
+	get_tree().change_scene_to_file("res://scenes/win.tscn")
---
+++ scripts/win.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var exit_button: Button = $VBoxContainer/ExitButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_exit_button_pressed() -> void:
+	Global.health = Global.max_health
+	Global.score = 0
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
