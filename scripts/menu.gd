04508d2637f05aebc77f5729b47bf45e45eafeae
juan-dio <juanaxl432@gmail.com>
2024-12-12 22:14:58 +0700

    feat(all): add credits and settings

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var start_button: Button = $VBoxContainer/StartButton
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Global.current_scene = "res://scenes/level_1.tscn"
+	Global.health = Global.max_health
+	Global.score = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +19,10 @@ func _on_instruction_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/story.tscn")
 
 func _on_setting_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,10 +1,9 @@
 extends Control
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.current_scene = "res://scenes/level_1.tscn"
-	Global.health = Global.max_health
-	Global.score = 0
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,10 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-func _on_try_again_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
-
-
-func _on_exit_button_pressed() -> void:
+func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
